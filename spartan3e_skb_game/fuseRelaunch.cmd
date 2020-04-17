6127b9feaae58e814b8dab28489a1fae8d10c552
Jakub Plona <plonajakub@gmail.com>
2020-04-17 14:41:10 +0200

    Code cleanup part 2

--- spartan3e_skb_game/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "C:/moje_pliki/Application_Projects/Studia_VI_semestr/UCiSW-projekt/spartan3e_skb_game/test_game_loop_state_machine_isim_beh.exe" -prj "C:/moje_pliki/Application_Projects/Studia_VI_semestr/UCiSW-projekt/spartan3e_skb_game/test_game_loop_state_machine_beh.prj" "work.test_game_loop_state_machine" 
